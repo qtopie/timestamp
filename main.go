58fb1c307f2eb7405c92ec152d7a034d00e3f6b9
qtopie <qtopie.rw@gmail.com>
2025-04-13 23:53:33 +0800

    fix err

--- main.go
+++ main.go
@@ -34,7 +34,8 @@ func parseTime(s string) time.Time {
 		log.Fatal(err)
 	}
 
-	if epoch < 1<<31 {
+	// 3000 AD (公元3000年)
+	if epoch < 32503651201 {
 		return time.UnixMilli(int64(epoch) * 1000)
 	} else {
 		return time.UnixMilli(int64(epoch))
@@ -53,8 +54,11 @@ func parseDuration(s string) time.Duration {
 			panic("required two time values for an interval")
 		}
 
-		t1 := parseTime((values[0]))
-		t2 := parseTime(values[1])
+		t1 := parseTime(strings.TrimSpace(values[0]))
+		t2 := parseTime(strings.TrimSpace(values[1]))
+
+		fmt.Println("First Moment:", t1)
+		fmt.Println("Second Moment:", t2)
 
 		if t1.After(t2) {
 			return t1.Sub(t2)
@@ -72,9 +76,21 @@ func parseDuration(s string) time.Duration {
 }
 
 func printDuration(d time.Duration) {
-	// s := ""
-	// if d.Abs().Hours() > 24 {
-	// 	s += string(d.Abs().Hours() / 24)
-	// }
-	fmt.Println("Duration:", d)
+	fmt.Println("Total Time:", d)
+
+	fmt.Print("Human-Readable Time:")
+	ms := d.Milliseconds()
+	if ms > 10*365*24*3600*1000 {
+		years := ms / (365 * 24 * 3600 * 1000)
+		fmt.Print(years, "Y")
+		ms -= years * (365 * 24 * 3600 * 1000)
+	}
+
+	if ms > 24*3600*1000 {
+		days := ms / (24 * 3600 * 1000)
+		fmt.Print(days, "D")
+		ms -= days * (24 * 3600 * 1000)
+	}
+	d, _ = time.ParseDuration(strconv.Itoa(int(ms)) + "ms")
+	fmt.Println(d)
 }
